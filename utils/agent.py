7a30ba16af543af4885c3759db098352333d1f65
daanniill <daniil.malakhov12@gmail.com>
2025-04-05 14:30:21 -0700

    defining env for agent

--- utils/agent.py
+++ utils/agent.py
@@ -1,4 +1,4 @@
-
+# defining agent
 from __future__ import annotations
 from livekit.agents import (
     AutoSubscribe,
@@ -12,3 +12,12 @@ from livekit.agents.multimodal import MultimodalAgent
 from livekit.plugins import openai
 from dotenv import load_dotenv
 import os
+
+# loading environment vars
+load_dotenv()
+
+# connects to a livekit room and subsribes to all the tracks in that room
+# aka in our case hears all audio in that room
+async def entrypoint(ctx: JobContext):
+    await ctx.connect(auto_subscribe=AutoSubscribe.SUBSCRIBE_ALL)
+    await ctx.wait_for_participant() # waits for a participant to join a room
