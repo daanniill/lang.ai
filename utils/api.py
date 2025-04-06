4b06268563fd0a49364ebafa90c80ee5809877e4
daanniill <daniil.malakhov12@gmail.com>
2025-04-05 17:06:52 -0700

    merging branches

--- utils/api.py
+++ utils/api.py
@@ -24,7 +24,7 @@ class TutorFnc(llm.FunctionContext):
         super().__init__()
         
         self._student_details = {
-            StudentDeatils.Student_id: "",
+            StudentDeatils.Student_id: 0,
             StudentDeatils.Name: "",
             StudentDeatils.Language: "",
             StudentDeatils.Skill_Level: "",
@@ -41,7 +41,48 @@ class TutorFnc(llm.FunctionContext):
         return student_str
     
     @llm.ai_callable(description="get details on current student")
-    def get_student_details(self):
-        logger.info("get student details")
+    def get_student_details(self, student_id: Annotated[str, llm.TypeInfo(description="The student id of the student to lookup")]):
+        logger.info("lookup student - student_id: %s", student_id)
+
+        result = db.get_student_by_id(student_id)
+        if result is None:
+            return "Student not found"
+        
+        self._student_details = {
+            StudentDeatils.Student_id: result.student_id,
+            StudentDeatils.Name: result.name,
+            StudentDeatils.Language: result.language,
+            StudentDeatils.Skill_Level: result.skill_level,
+            StudentDeatils.Strengths: result.strengths,
+            StudentDeatils.Weaknesses: result.weaknesses,
+        }
+
         return f"The student details are: {self.get_student_str()}"
+    
+    def add_student(
+        self, 
+        student_id: Annotated[int, llm.TypeInfo(description="The student id of the student")],
+        name: Annotated[str, llm.TypeInfo(description="The name of the student")],
+        email: Annotated[str, llm.TypeInfo(description="The email of the student")],
+        phone_number: Annotated[str, llm.TypeInfo(description="The email of the student")],
+        language: Annotated[str, llm.TypeInfo(description="The language of the student")],
+        skill_level: Annotated[str, llm.TypeInfo(description="The skill level of the student")],
+        strengths: Annotated[str, llm.TypeInfo(description="The strengths of the student")],
+        weaknesses: Annotated[str, llm.TypeInfo(description="The weaknesses of the student")],
+    ):
+        logger.info("add student - student id: %s, name: %s, email: %s, phone_number: %s, language: %s, skill level: %s, strengths: %s, weaknesses: %s", student_id, name, email, phone_number, language, skill_level, strengths, weaknesses)
+        result = db.add_student(student_id, name, email, phone_number, language, skill_level, strengths, weaknesses)
+        if result is None:
+            return "Failed to add student"
+        
+        self._student_details = {
+            StudentDeatils.Student_id: result.student_id,
+            StudentDeatils.Name: result.name,
+            StudentDeatils.Language: result.language,
+            StudentDeatils.Skill_Level: result.skill_level,
+            StudentDeatils.Strengths: result.strengths,
+            StudentDeatils.Weaknesses: result.weaknesses,
+        }
+
+        return "Student Added!"
 
